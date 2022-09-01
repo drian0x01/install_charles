36d90e4fe2e375dcda70c283ddbd4cb4fb00cb70
Drian <11167300+drian0x01@users.noreply.github.com>
2022-09-01 03:59:36 -0400

    Create install-charles.sh

---
+++ install-charles.sh
@@ -0,0 +1,24 @@
+#!/bin/bahs
+# author: Drian Freitas <drian.io>
+# collab: Luis Teixeira
+# Simple script to install charles proxy
+
+# Baixar charles proxy
+wget --quiet --show-progress  "https://www.charlesproxy.com/assets/release/4.6.2/charles-proxy-4.6.2_amd64.tar.gz?k=616ff66d1b"
+
+# extrair o arquivo .tar.gz
+tar -xf charles-proxy-4.6.2_amd64.tar.gz
+
+# Mover arquivos forçadamente para a dir /opt/
+rsync -a charles /opt/ --remove-sent-files --ignore-existing --whole-file
+
+# Atualizar arquivo charles-proxy.desktop com dir absoluto da localização do ícone do Charles
+sed -i 's+../icon+/opt/charles/icon+g' /opt/charles/etc/charles-proxy.desktop
+
+# Criar link simbólico
+sudo ln -s /opt/charles/etc/charles-proxy.desktop /usr/share/applications/charles.desktop
+
+# Fazer com o ambiente reconheça as alterações feitas
+source ~/.bashrc
+
+echo "Charles Proxy instalado"
